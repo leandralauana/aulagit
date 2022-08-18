1a5705ac3b019d317720b36c12c55396721ed817
leandralauana <leandralauana19@gmail.com>
2022-08-18 09:02:09 -0300

    FATORIAL E ARQUIVO .GITIGNORE

--- git.cpp
+++ git.cpp
@@ -1,4 +1,15 @@
 #include<stdio.h>
+int fatorial(int n){
+    if(n == 0){
+        return 1;
+    }else{
+        return n*fatorial(n-1);
+    }
+}
 int main(){
     printf("Hello Git\n");
+    printf("Fatorial(5) = %d\n", fatorial(5));
 }
+
+llll
+
